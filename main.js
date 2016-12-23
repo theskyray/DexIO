a8756e1b3dfad35ec7673a9d4343b03b40efc018
theskyray <german@iMac-de-German.local>
2016-12-24 00:58:03 +0100

    plantilla

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const Menu = electron.Menu;
 const remote = require('electron').remote;
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width:1355, height: 498, titleBarStyle: 'hidden-inset', backgroundColor: '#EF5350'});
+  let win = new BrowserWindow({width:1280, height: 720, backgroundColor: '#EF5350'});
   win.loadURL(`file://${__dirname}/index.html`);
   win.webContents.openDevTools();
 });
