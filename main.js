7f35a2f7d77871c217dbb8a9651c9c6f4526c2d8
theskyray <german@Kodi.local>
2017-03-13 23:17:55 +0100

    Rediseño y 2 nuevas funciones

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const electron = require('electron');
 const {app, BrowserWindow} = electron;
 const path = require('path')
 const Menu = electron.Menu;
-const remote = require('electron').remote;
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width:1280, height: 720, backgroundColor: '#EF5350'});
+  let win = new BrowserWindow({width:1280, height: 720, frame: false, titleBarStyle: 'hidden'});
   win.loadURL(`file://${__dirname}/index.html`);
-  win.webContents.openDevTools();
 });
